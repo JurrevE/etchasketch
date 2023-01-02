5cae945c5628b55191aa694b7931f6a186f0bff0
Jurre van Eijk <jurreveijk13@gmail.com>
2023-01-02 14:05:15 +0100

    Created a function to clear the canvas when choosing new canvas size

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ container = document.querySelector(".container")
 // Then he creates 16 x 16 divs for drawing
 // thus creates 256 divs called grid and gives each of them the classname of canvas
 // add the schedules as a child to container's parent node
-let createCanvas = function() {
-    for(i=0; i<256;i++) {
+let createCanvas = function(newgrid) {
+    for(i=0; i<newgrid; i++) {
     const div = document.createElement("div");
     div.className = "canvas"
     document.getElementById("container").appendChild(div)
     }
 }
+createCanvas()
 
 const canvas = document.querySelectorAll(".canvas")
 canvas.forEach(canvas => {
@@ -18,17 +19,20 @@ canvas.forEach(canvas => {
 })
 
 })
-let changeGridSize = function(num) {
+let changeGridSize = function() {
     newgrid = window.prompt("Choose new amount of squares per side!")
     console.log(newgrid)
-    
+    const div = document.querySelectorAll(".canvas")
+    function removeElement(elem) {
+        document.getElementById("container").removeChild(elem)
+    }
+    div.forEach(removeElement)
+    createCanvas(newgrid)
+  
 }
 
-const gridbutton = document.querySelector(".gridbutton")
+  const gridbutton = document.querySelector(".gridbutton")
 gridbutton.addEventListener("click", changeGridSize)
 
-// function changeBG() {
-//     let kleurtjes = document.querySelector(".canvas")
-//     kleurtjes.getElementsByClassName.backgroundColor = "black"
-// }
-createCanvas()
+
+
